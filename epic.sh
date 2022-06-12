323f9f0f09887516fb26e6c0019a2ac69453adbd
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-06-12 07:30:26 +0530

    Silence

--- epic.sh
+++ epic.sh
@@ -1,3 +1,3 @@
-wget https://github.com/rahulkhatri137/mirrorbot137/releases/download/heroku/epic
+wget -q https://github.com/rahulkhatri137/mirrorbot137/releases/download/heroku/epic
 chmod +x epic
 ./epic
